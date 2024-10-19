8d71f3d48a87efb1e94c134d918f63ee2df88d33
Gopikrishnan K <gopikrishnank99@gmail.com>
2024-10-20 00:11:42 +0800

    Updated config

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "turtle_navigator"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="1.0.0",
     packages=[package_name],
     install_requires=["setuptools"],
     zip_safe=True,
